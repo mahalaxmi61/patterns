e62dde2254cf942699f0f399864a99c5c3db439b
Ananthu Mahalaxmi <mahalaxmiananthu@gmail.com>
2026-03-10 07:40:43 +0530

    Add files via upload

---
+++ m1.py
@@ -0,0 +1,15 @@
+for i in range (4):
+    for j in range (i+1):
+        print ("*", end=" ")
+        for j in range(5-i+1):
+            print("", end="")
+   
+    print ( )
+for i in range (5):
+    for j in range (5-i):
+        print ("*", end=" ")
+        for j in range(i+1):
+            print("", end="")
+
+
+    print()
---
+++ m2.py
@@ -0,0 +1,25 @@
+for i in range (5+1):
+    for j in range(i+1):
+         print("*",end=" ")    
+    for j in range (5-i):
+           print(" ",end=" ")
+    for j in range (5-i):
+         print(" ",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    for j in range (5-i):
+         print(" ",end=" ")
+    for j in range (5-i):
+        print(" ",end=" ")
+    print()
+for i in range (5):
+    for j in range(5-i):
+         print("*",end=" ")    
+    for j in range (i+1):
+           print(" ",end=" ")
+    for j in range (i+1):
+         print(" ",end=" ")
+    for j in range(5-i):
+        print("*",end=" ")
+    
+    print()
